8d7d920f329d5242309e6cc7a38e961c56d2d313
KhushPatibandha <khush.patibandha@gmail.com>
2024-08-13 00:48:13 +0530

    --

---
+++ cp31/q900/D_BalancedRound.java
@@ -0,0 +1,47 @@
+package cp31.q900;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class D_BalancedRound {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int j = 0; j < t; j++) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            Arrays.sort(arr);
+            int l = 0;
+            int r = 0;
+            int maxCount = 0;
+            int count = 1;
+
+            while(r < n - 1) {
+                if(Math.abs(arr[r] - arr[r + 1]) <= k) {
+                    r++;
+                } else {
+                    count = r - l + 1;
+                    r++;
+                    l = r;
+                    if(maxCount < count) {
+                        maxCount = count;
+                    }
+                }
+            }
+            count = r - l + 1;
+            r++;
+            l = r;
+            if(maxCount < count) {
+                maxCount = count;
+            }
+            // System.out.println(maxCount + " m c");
+            System.out.println(n - maxCount);
+        }
+        sc.close();
+    }
+}
